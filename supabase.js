12b5dc577fff51a656b639374c01912e8fbdc1b2
manojdashaundi-svg <manojdashaundi@gmail.com>
2026-07-24 19:02:42 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,13 @@
-// Supabase configuration will be added here.
+// Supabase Library बाट Client बनाउने
+
+const SUPABASE_URL = "https://lorhrfdqzagulicwcctx.supabase.co";
+
+const SUPABASE_ANON_KEY = "तपाईंको sb_publishable_... key यहाँ राख्नुहोस्";
+
+// Supabase Connect
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
+
+console.log("Supabase Connected");
