43cc35f9244a2c113667741a8aa03ef2fda1bb21
manojdashaundi-svg <manojdashaundi@gmail.com>
2026-07-24 20:12:28 +0800

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 // Supabase Library बाट Client बनाउने
 
-const SUPABASE_URL = "https://YOUR_PROJECT.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://xxxxxxxxxxxxxxxx.supabase.co";
+const SUPABASE_ANON_KEY = "तपाईंको_वास्तविक_publishable_anon_key";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
